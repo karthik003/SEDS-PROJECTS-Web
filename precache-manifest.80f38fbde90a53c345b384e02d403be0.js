0de2203fe65e1388badb38d8b2f7fcfdcbf7b9a6
Karthik V.J <vjk2018@gmail.com>
2020-06-06 00:47:50 +0530

    Updates

---
+++ precache-manifest.80f38fbde90a53c345b384e02d403be0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6579c5d4af4e686baf68e539c258adb",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "7a9c897fc6f9183075aa",
+    "url": "/web/static/css/2.3d67f493.chunk.css"
+  },
+  {
+    "revision": "24b8ba9363b0437ec892",
+    "url": "/web/static/css/main.a1b285d2.chunk.css"
+  },
+  {
+    "revision": "7a9c897fc6f9183075aa",
+    "url": "/web/static/js/2.31f37fa4.chunk.js"
+  },
+  {
+    "revision": "c42a0f8f67c7b7d789b5b8875326f598",
+    "url": "/web/static/js/2.31f37fa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24b8ba9363b0437ec892",
+    "url": "/web/static/js/main.da308086.chunk.js"
+  },
+  {
+    "revision": "6be78bf9b12e1cabf9c3",
+    "url": "/web/static/js/runtime-main.3b1e144e.js"
+  },
+  {
+    "revision": "6eb936fd7a105a3d13085620318ed186",
+    "url": "/web/static/media/1.6eb936fd.jpeg"
+  },
+  {
+    "revision": "c2b42821eda6e3e2034831f2c44bfa38",
+    "url": "/web/static/media/ardra.c2b42821.png"
+  },
+  {
+    "revision": "ba447a7ebd2321df7de0cd1cfa7a9741",
+    "url": "/web/static/media/astro.ba447a7e.webp"
+  },
+  {
+    "revision": "c9ac7de64acb0ccb565b6885e3c49db7",
+    "url": "/web/static/media/black2.c9ac7de6.jpg"
+  },
+  {
+    "revision": "325eed7eda968e4e81e25a41177896af",
+    "url": "/web/static/media/down.325eed7e.gif"
+  },
+  {
+    "revision": "15165acc6aa088491f365c598363eb7d",
+    "url": "/web/static/media/seds-white.15165acc.png"
+  },
+  {
+    "revision": "a4cce18318ae52262c108aa190168bda",
+    "url": "/web/static/media/stars.a4cce183.webp"
+  },
+  {
+    "revision": "9c4b7eb30b492e21b075dd0593305383",
+    "url": "/web/static/media/vyadh.9c4b7eb3.png"
+  }
+]);
