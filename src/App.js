2af41357f0bada0b08a1fbaa4fbfdd8dcf91775d
Karthik V.J <vjk2018@gmail.com>
2020-06-13 04:25:06 +0530

    Added Service-worker

--- src/App.js
+++ src/App.js
@@ -7,8 +7,27 @@ import About from './Components/About';
 import Contact from './Components/Contact';
 import Teams from './Components/Teams';
 import Conquests from './Components/Conquests';
+import loader from './Assets/loader1.gif'
+class App extends React.Component{
 
-function App() {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            loading: true,
+
+        }
+    }
+render() {
+  const { loading } = this.state;
+    
+  if(!loading) { // if your component doesn't have to wait for an async action, remove this block 
+    return(
+      <div style={{backgroundColor:"black",margin:"0px auto",height:"100vh"}}>    
+           <img src={loader} alt="loading..." style={{margin:"0px 25%"}}/>
+      </div>
+    ); // render null when app is not ready
+  }
   return (
     <div style={{backgroundColor:"black"}}>
     
@@ -16,6 +35,7 @@ function App() {
      
     </div>
   );
+}
 }
 
 export default App;
