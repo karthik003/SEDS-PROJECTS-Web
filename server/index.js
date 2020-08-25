56f1f3ce64292a18d87bb91bbc7bd88a343a2020
Karthik V.J <vjk2018@gmail.com>
2020-08-25 15:24:14 +0530

    Index.html

--- server/index.js
+++ server/index.js
@@ -7,6 +7,6 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
-app.listen(3001, () =>
+app.listen(process.env.PORT || 3001, () =>
   console.log('Express server is running on localhost:3001')
 );
