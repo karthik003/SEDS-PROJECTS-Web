0c5508aafa82331eb951ccb8edbacd12a69b4103
sushant4191 <metalsushant@gmail.com>
2022-01-13 00:45:50 +0545

    board positions

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -30,21 +30,21 @@ class Board extends Component {
                         <Boardmember image={vc} instagram='/instavicecap'linkedin="/linkvc" name="Abhishek" position="VICE CAPTAIN"/>
                         <Boardmember image={f} instagram='/instafinance' linkedin="/linkf" name="Akalpit Dawkhar" position="FINANCE HEAD"/>
                         <Boardmember image={mgh} instagram='/instamgmt' linkedin="/linkmgh" name="Shivika Bisaria" position="MANAGEMENT HEAD"/>
-                        <Boardmember image={mgch} instagram='/instamgmtco' linkedin="/linkmgch" name="Aehsas Mathur" position="MANAGEMENT CO-HEAD"/>
+                        {/* <Boardmember image={mgch} instagram='/instamgmtco' linkedin="/linkmgch" name="Aehsas Mathur" position="MANAGEMENT CO-HEAD"/> */}
                     </Row>
                     <Row style={{margin:"0 5% 0 5%",justifyContent:"center"}} >
                     <Boardmember image={csh} instagram='/instasoftware' linkedin="/linkcsh"name="Roopesh A" position="SOFTWARE HEAD"/>
-                    <Boardmember image={csch} instagram='/instasoftwareco' linkedin="/linkcsch"name="Shaolin Kataria" position="SOFTWARE CO-HEAD"/>
+                    {/* <Boardmember image={csch} instagram='/instasoftwareco' linkedin="/linkcsch"name="Shaolin Kataria" position="SOFTWARE CO-HEAD"/> */}
                     <Boardmember image={eh} instagram='/instaele' linkedin="/linkeh"name="Shwetanshu Singh" position="ELECTRICAL HEAD"/>
-                    <Boardmember image={ech1} instagram='/instaeleco1' linkedin="/linkech1"name="Dhruv Shah" position="ELECTRICAL CO-HEAD"/>
-                    <Boardmember image={ech2} instagram='/instaeleco2' linkedin="/linkech2"name="Karthik Rajesh" position="ELECTRICAL CO-HEAD"/>
+                    {/* <Boardmember image={ech1} instagram='/instaeleco1' linkedin="/linkech1"name="Dhruv Shah" position="ELECTRICAL CO-HEAD"/> */}
+                    {/* <Boardmember image={ech2} instagram='/instaeleco2' linkedin="/linkech2"name="Karthik Rajesh" position="ELECTRICAL CO-HEAD"/> */}
                     </Row>
                     <Row style={{margin:"0 5% 0 5%",justifyContent:"center"}} >
                     <Boardmember image={mh} instagram='/instamech' linkedin="/linkmh"name="Pratush Sidar" position="MECHANICAL HEAD"/>
-                    <Boardmember image={mch} instagram='/instamechco'linkedin="/linkmch" name="Aditya Murali" position="MECHANICAL CO-HEAD"/>
+                    {/* <Boardmember image={mch} instagram='/instamechco'linkedin="/linkmch" name="Aditya Murali" position="MECHANICAL CO-HEAD"/> */}
                     <Boardmember image={rdh} instagram='/instarad'linkedin="/linkrdh" name="Darshan Ram" position="R&D HEAD"/>
-                    <Boardmember image={rdch1} instagram='/instaradco1'linkedin="/linkrdch1" name="V.J.Karthik" position="R&D CO-HEAD"/>
-                    <Boardmember image={rdch2} instagram='/instaradco2' linkedin="/linkrdch2"name="Aditya Ray" position="R&D CO-HEAD"/>
+                    {/* <Boardmember image={rdch1} instagram='/instaradco1'linkedin="/linkrdch1" name="V.J.Karthik" position="R&D CO-HEAD"/> */}
+                    {/* <Boardmember image={rdch2} instagram='/instaradco2' linkedin="/linkrdch2"name="Aditya Ray" position="R&D CO-HEAD"/> */}
                     </Row>
                     
             </div>
