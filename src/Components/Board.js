0a757a3d9bb0d600625be31aa4413a2c07e07741
sushant4191 <metalsushant@gmail.com>
2022-01-13 00:49:40 +0545

    board_final

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -38,14 +38,16 @@ class Board extends Component {
                     <Boardmember image={eh} instagram='/instaele' linkedin="/linkeh"name="Shwetanshu Singh" position="ELECTRICAL HEAD"/>
                     {/* <Boardmember image={ech1} instagram='/instaeleco1' linkedin="/linkech1"name="Dhruv Shah" position="ELECTRICAL CO-HEAD"/> */}
                     {/* <Boardmember image={ech2} instagram='/instaeleco2' linkedin="/linkech2"name="Karthik Rajesh" position="ELECTRICAL CO-HEAD"/> */}
+                    <Boardmember image={mh} instagram='/instamech' linkedin="/linkmh"name="Pratush Sidar" position="MECHANICAL HEAD"/>
+                    <Boardmember image={rdh} instagram='/instarad'linkedin="/linkrdh" name="Darshan Ram" position="R&D HEAD"/>
                     </Row>
-                    <Row style={{margin:"0 5% 0 5%",justifyContent:"center"}} >
+                    {/* <Row style={{margin:"0 5% 0 5%",justifyContent:"center"}} >
                     <Boardmember image={mh} instagram='/instamech' linkedin="/linkmh"name="Pratush Sidar" position="MECHANICAL HEAD"/>
                     {/* <Boardmember image={mch} instagram='/instamechco'linkedin="/linkmch" name="Aditya Murali" position="MECHANICAL CO-HEAD"/> */}
                     <Boardmember image={rdh} instagram='/instarad'linkedin="/linkrdh" name="Darshan Ram" position="R&D HEAD"/>
                     {/* <Boardmember image={rdch1} instagram='/instaradco1'linkedin="/linkrdch1" name="V.J.Karthik" position="R&D CO-HEAD"/> */}
                     {/* <Boardmember image={rdch2} instagram='/instaradco2' linkedin="/linkrdch2"name="Aditya Ray" position="R&D CO-HEAD"/> */}
-                    </Row>
+                    </Row> */}
                     
             </div>
         )
